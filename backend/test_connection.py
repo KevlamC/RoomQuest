03f39c7fb3d7c811c3f4e7dc8a9389a41e53a23b
KevlamC <149285471+KevlamC@users.noreply.github.com>
2025-04-15 22:42:44 -0600

    Update test_connection.py

--- backend/test_connection.py
+++ backend/test_connection.py
@@ -1,11 +1,16 @@
+from flask import Blueprint
 from config import get_connection
 
-try:
-    conn = get_connection()
-    if conn.is_connected():
-        print("✅ Successfully connected to the database!")
-    else:
-        print("❌ Connection failed.")
-    conn.close()
-except Exception as e:
-    print(f"❌ Error: {e}")
+test_connection_bp = Blueprint("test_connection", __name__)
+
+@test_connection_bp.route("/test-connection")
+def test_connection():
+    try:
+        conn = get_connection()
+        if conn.is_connected():
+            conn.close()
+            return "✅ Successfully connected to the database!"
+        else:
+            return "❌ Connection failed."
+    except Exception as e:
+        return f"❌ Error: {e}"
