90e3566d21e10e5f9216415446d2f71f4b266413
KevlamC <kevlam.chundawat@gmail.com>
2025-04-17 04:08:14 -0600

    Especially to connect with Wix frontend with the right wix site link

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from .routes.test_connection_route import test_connection_bp  # This is also a r
 from .routes.init_db_route import init_db_bp  # Import the init_db route
 
 app = Flask(__name__)  # make sure this comes first!
-CORS(app, origins=["https://*.wixsite.com", "http://localhost:5173"], methods=["GET", "POST", "OPTIONS"])
+CORS(app, origins=["https://yashdhaneshwari.wixsite.com", "http://localhost:5173"], methods=["GET", "POST", "OPTIONS"])
 
 # Register blueprints
 app.register_blueprint(rooms_bp, url_prefix="/api/rooms")
