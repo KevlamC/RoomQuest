bde184670c6268fe4ec950a5ee3e385667077d71
KevlamC <149285471+KevlamC@users.noreply.github.com>
2025-04-19 13:04:10 -0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from .routes.search_rooms import rooms_bp  # This is now a relative import
 from backend.config import get_connection
 from .routes.test_connection_route import test_connection_bp  # This is also a relative import
 from .routes.init_db_route import init_db_bp  # Import the init_db route
-from .routes.signup_bp import signup_bp
+from .routes.signup_db import user_bp
 from .routes.user_list_bp import user_list_bp
 from .routes.eventsUpcoming import eventsUpcoming
 from .routes.points import points
@@ -18,7 +18,7 @@ CORS(app, origins=["https://yashdhaneshwari.wixsite.com", "http://localhost:5173
 app.register_blueprint(rooms_bp, url_prefix="/api/rooms")
 app.register_blueprint(test_connection_bp) 
 app.register_blueprint(init_db_bp)
-app.register_blueprint(signup_bp)
+app.register_blueprint(user_bp)
 app.register_blueprint(user_list_bp)
 app.register_blueprint(eventsUpcoming)
 app.register_blueprint(points)
