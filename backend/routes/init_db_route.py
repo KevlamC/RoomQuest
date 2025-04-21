d874510291fe4e47193bc2c2549fb50e68683205
KevlamC <149285471+KevlamC@users.noreply.github.com>
2025-04-21 10:26:47 -0600

    Update init_db_route.py

--- backend/routes/init_db_route.py
+++ backend/routes/init_db_route.py
@@ -127,7 +127,7 @@ def init_db():
           IsPublic     BOOLEAN       DEFAULT TRUE,
           Link         VARCHAR(255)  NULL,
           EventType    ENUM('club', 'university') NOT NULL,
-          ADD COLUMN ClubID INT NULL,
+          ClubID INT NULL,
           FOREIGN KEY (ClubID) REFERENCES CLUB(ID) ON DELETE SET NULL ON UPDATE CASCADE;
           FOREIGN KEY (BookingID)    REFERENCES TIME_SLOT(BookingID)
               ON DELETE CASCADE
