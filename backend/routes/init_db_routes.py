2899c96de5578cc7184bcce4c4d45123e1ad982f
Devanshi Iragavarapu <devanshi.iragavar1@ucalgary.ca>
2025-04-22 06:04:04 -0600

    re-deploy

--- backend/routes/init_db_routes.py
+++ backend/routes/init_db_routes.py
@@ -27,7 +27,7 @@ def init_db():
         cursor.execute("DROP TABLE IF EXISTS USER;")
         cursor.execute("DROP TABLE IF EXISTS ROOMS;")
         cursor.execute("DROP TABLE USER_EVENT_TOPIC_PREFS")
-
+        
         # Recreate all tables
         cursor.execute("""
         CREATE TABLE USER (
